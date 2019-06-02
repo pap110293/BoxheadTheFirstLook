de26597e3173d07808521370228cd703e23a30ab
vinhld <ldvinh@hotmail.com>
2019-06-02 15:36:53 +0700

    rename class

--- Assets/Scripts/Enemy/EnemySkillSound.cs
+++ Assets/Scripts/Enemy/EnemySkillSound.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySound : MonoBehaviour
+public class EnemySkillSound : MonoBehaviour
 {
     public AudioClip idleSound;
     public AudioClip attackSound;
