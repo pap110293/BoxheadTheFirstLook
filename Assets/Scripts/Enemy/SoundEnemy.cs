2c04b5de1f2c01a371459ab3ae4e40c883758753
vinhld <ldvinh@hotmail.com>
2019-06-02 16:27:39 +0700

    update

---
+++ Assets/Scripts/Enemy/SoundEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEnemy : MonoBehaviour
+{
+    public AudioClip idleSound;
+    public AudioSource audioSource;
+    Coroutine coroutine = null;
+    void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+        if (coroutine != null) StopCoroutine(coroutine);
+        //coroutine = StartCoroutine(SoundPlay(10.0f));
+    }
+    IEnumerator SoundPlay(float _delayTime)
+    {
+        while (true)
+        {
+            if (audioSource && idleSound)
+            {
+                audioSource.clip = idleSound;
+                audioSource.loop = false;
+                audioSource.Play();
+            }
+            yield return new WaitForSecondsRealtime(_delayTime);
+        }
+    }
+    private void OnDestroy()
+    {
+        if (coroutine != null) StopCoroutine(coroutine);
+    }
+}
