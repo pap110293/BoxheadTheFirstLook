9d8dd180bdda894aeede53877c8de85afee7f7cf
kjmgjaljnh <meilin1195@gmail.com>
2019-06-03 20:56:15 +0700

    music

---
+++ Assets/Scripts/HUB/MainMenu/RotateSky.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSky : MonoBehaviour
+{
+    float rotateSpeed = 2.0f;
+   
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotateSpeed);
+    }
+}
